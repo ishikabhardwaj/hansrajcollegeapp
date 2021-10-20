cc1738abecda9e1cb961b4d7bd85887c7ffd3566
hjaiswal1003 <harshitjaiswal03082001@gmail.comgit config --global user.name hjaiswal1003git config --global user.email harshitjaiswal03082001@gmail.com>
2021-10-20 17:08:38 +0530

    Teacher Upload Attendance Module Updated

---
+++ app/src/main/java/com/example/hansrajcollege/RecyclerAdapterForUploadAtt.java
@@ -0,0 +1,59 @@
+package com.example.hansrajcollege;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class RecyclerAdapterForUploadAtt extends RecyclerView.Adapter<RecyclerAdapterForUploadAtt.ViewHolder> {
+   private ArrayList<TeacherUploadAttendance> data;
+
+    // RecyclerView recyclerView;
+    public RecyclerAdapterForUploadAtt(ArrayList<TeacherUploadAttendance> data) {
+        this.data = data;
+    }
+
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View listItem= layoutInflater.inflate(R.layout.activity_listview_attendance_upload, parent, false);
+        ViewHolder viewHolder = new ViewHolder(listItem);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        final TeacherUploadAttendance myListData = data.get(position);
+        holder.textView1.setText(myListData.getmName());
+        holder.textview2.setText(myListData.getmCourse());
+        int roll = myListData.getmRollNo();
+        String r = Integer.toString(roll);
+        holder.textview3.setText(r);
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView textView1,textview2,textview3;
+        public ViewHolder(View itemView) {
+            super(itemView);
+            this.textView1 = (TextView) itemView.findViewById(R.id.textView2);
+            this.textview2 = (TextView) itemView.findViewById(R.id.textView1);
+            this.textview3 = (TextView) itemView.findViewById(R.id.textView3);
+
+        }
+    }
+}
+
+
---
+++ app/src/main/java/com/example/hansrajcollege/TeacherAttendance.java
@@ -0,0 +1,50 @@
+package com.example.hansrajcollege;
+
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+
+public class TeacherAttendance extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View root=inflater.inflate(R.layout.fragment_upload_attendance, container, false);
+        final ArrayList<TeacherUploadAttendance> words= new ArrayList<TeacherUploadAttendance>();
+        words.add(new TeacherUploadAttendance("Vardha Jain","B.Sc(Hons.) Computer Science",8151));
+        words.add(new TeacherUploadAttendance("Harshit Jaiswal","B.Sc(Hons.) Computer Science",8152));
+        words.add(new TeacherUploadAttendance("Monika Joshi","B.Sc(Hons.) Computer Science",8153));
+        words.add(new TeacherUploadAttendance("Hemant Giri Goshwami","B.Sc(Hons.) Computer Science",8154));
+        words.add(new TeacherUploadAttendance("Priyanka Das","B.Sc(Hons.) Computer Science",8155));
+        words.add(new TeacherUploadAttendance("Mamidi Chandu","B.Sc(Hons.) Computer Science",8157));
+        words.add(new TeacherUploadAttendance("Benika Yadav","B.Sc(Hons.) Computer Science",8158));
+        words.add(new TeacherUploadAttendance("Benika Yadav","B.Sc(Hons.) Computer Science",8158));
+        words.add(new TeacherUploadAttendance("Benika Yadav","B.Sc(Hons.) Computer Science",8158));
+        words.add(new TeacherUploadAttendance("Benika Yadav","B.Sc(Hons.) Computer Science",8158));
+        words.add(new TeacherUploadAttendance("Benika Yadav","B.Sc(Hons.) Computer Science",8158));
+        words.add(new TeacherUploadAttendance("Benika Yadav","B.Sc(Hons.) Computer Science",8158));
+        words.add(new TeacherUploadAttendance("Benika Yadav","B.Sc(Hons.) Computer Science",8158));
+        words.add(new TeacherUploadAttendance("Benika Yadav","B.Sc(Hons.) Computer Science",8158));
+        RecyclerView list=(RecyclerView) root.findViewById(R.id.list);
+        list.setLayoutManager(new LinearLayoutManager(getActivity()));
+        list.setAdapter(new RecyclerAdapterForUploadAtt(words));
+        DividerItemDecoration horizontalDecoration = new DividerItemDecoration(list.getContext(),
+                DividerItemDecoration.VERTICAL);
+        Drawable horizontalDivider = ContextCompat.getDrawable(getActivity(), R.drawable.vertical_divider);
+        horizontalDecoration.setDrawable(horizontalDivider);
+        list.addItemDecoration(horizontalDecoration);
+        return root;
+    }
+
+}
